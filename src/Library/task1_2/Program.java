1feb2ac4d96cf9e271030ff1bcea26ad97bace48
Sandman41k <logush.vlad@mail.ru>
2026-08-07 14:50:10 +0300

    сделал программу в три строки

---
+++ src/Library/task1_2/Program.java
@@ -0,0 +1,9 @@
+package Library.task1_2;
+
+public class Program {
+    public static void main (String[] args) {
+        System.out.println("Владислав");
+        System.out.println("30");
+        System.out.println("Java");
+    }
+}
