5bc53a1ca08a387e0237b83d68d6104897e4d1a6
Sandman41k <logush.vlad@mail.ru>
2026-08-07 16:42:30 +0300

    сделал третье зажание

---
+++ src/Library/task1_3/Program.java
@@ -0,0 +1,15 @@
+package Library.task1_3;
+
+public class Program {
+    public static void main (String[] args){
+        System.out.println("Владислав");
+        System.out.println(123 * 456);
+        System.out.println("*".repeat(10));
+        System.out.print("Выводит ");
+        System.out.print("несколько ");
+        System.out.print("слов ");
+        System.out.print("в ");
+        System.out.print("одну ");
+        System.out.print("строку");
+    }
+}
